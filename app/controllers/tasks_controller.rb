5e9d155a76744d0eec994448679117e9ad8f55d5
masaki.takahashi <yubugyu725@cream.pink>
2020-06-23 15:28:12 +0000

    L15-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show, :new]
+  before_action :require_user_logged_in
+  before_action :correct_user, only:[:show]
   def index
     if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
@@ -60,5 +61,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status, :user_id)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
